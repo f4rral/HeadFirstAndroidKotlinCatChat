1ed10a69ecd4fb389eb5fb633534a0694ef2043f
f4rral <f4rral@gmail.com>
2024-09-29 16:51:03 +0300

    Side Panel

--- app/src/main/java/com/hfad/catchat/MainActivity.kt
+++ app/src/main/java/com/hfad/catchat/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +27,26 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val builder = AppBarConfiguration.Builder(navController.graph)
-        val appBarConfiguration = builder.build()
-        toolbar.setupWithNavController(navController, appBarConfiguration)
 
         // Связывание нижней панели с контроллером навигации
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavView.setupWithNavController(navController)
+
+        // Получение ссылки на выдвижную панель
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        // Добавление выдвижной панели в AppBarConfiguration
+        // На панели инструментов выводится значок для открытия выдвижной панели.
+        // Значок выдвижной панели будет отображаться на всех экранах без кнопки Up
+        builder.setOpenableLayout(drawer)
+
+        val appBarConfiguration = builder.build()
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Навигация по щелчку на элементах включается связыванием
+        // выдвижной панели с контроллером навигации
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
